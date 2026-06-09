eb2dc6c063916c8937a4fabc8456e71c4cdd6cd4
luiztestonidev <luiztestoni5@gmail.com>
2026-06-09 14:34:10 -0300

    feat: criar entidade Cliente com encapsulamento e CPF imutável

---
+++ src/main/java/com/oficina/model/Cliente.java
@@ -0,0 +1,30 @@
+package com.oficina.model;
+
+public class Cliente {
+
+    private String nome;
+    private final String cpf;
+    private String telefone;
+
+    public Cliente(String nome, String cpf, String telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
