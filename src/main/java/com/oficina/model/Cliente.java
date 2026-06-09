14a8f92926710436d1ae550659140291fb59f6b2
luiztestonidev <luiztestoni5@gmail.com>
2026-06-09 15:17:29 -0300

    feat: adicionar relacionamento entre Cliente e veiculo

--- src/main/java/com/oficina/model/Cliente.java
+++ src/main/java/com/oficina/model/Cliente.java
@@ -1,15 +1,28 @@
 package com.oficina.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cliente {
 
     private String nome;
     private final String cpf;
     private String telefone;
+    private List<Veiculo> veiculos;
 
     public Cliente(String nome, String cpf, String telefone) {
         this.nome = nome;
         this.cpf = cpf;
         this.telefone = telefone;
+        this.veiculos = new ArrayList<>();
+    }
+
+    public void adicionarVeiculo(Veiculo veiculo) {
+        this.veiculos.add(veiculo);
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
     }
 
     public String getNome() {
